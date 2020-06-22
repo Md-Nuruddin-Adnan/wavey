baec639bfacc58c79c35ea1e410ed0aeaf221782
Md. Nuruddin Adnan <nuruddin.adnan1993@gmail.com>
2020-06-22 17:49:13 +0600

    index page with responsive

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,19 @@
 "use strict";
 
-//login-registration slide
+// tol tip activation
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
 
+//======== scroll off when side menu open
+
+
+//========= end of scroll off when side menu open
+
+
+
+
+//login-registration slide
 $('.login-reg-slide').slick({
   infinite: true,
   dots: true,
@@ -27,3 +39,94 @@ function closeNav() {
 $(".static-btn").click(function(){
   $(".static-btn i").toggleClass("rotate");
 });
+
+
+//live-streaming-slide of home
+$('.streaming-slide').slick({
+  dots: false,
+  arrows: true,
+  prevArrow: '<span class="prev-arrow"><i class="far fa-chevron-left"></i></span>',
+  nextArrow: '<span class="next-arrow"><i class="far fa-chevron-right"></i></span>',
+  autoplay: false,
+  slidesToShow: 10,
+  slidesToScroll: 5,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 7,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      }
+    },
+  ]
+}); 
+
+
+//suggestion-slide of home
+$('.suggestion-slide').slick({
+  dots: false,
+  arrows: true,
+  prevArrow: '<span class="prev-arrow"><i class="far fa-chevron-left"></i></span>',
+  nextArrow: '<span class="next-arrow"><i class="far fa-chevron-right"></i></span>',
+  autoplay: false,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 321,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+  ]
+}); 
+
+
+// grid view div 
+function gridView(){
+  $(".grid-list-col").removeClass("col-md-12");
+  $(".grid-list-col").addClass("col-md-6");
+  $(".grid-btn").addClass("active");
+  $(".list-btn").removeClass("active");
+}
+
+// list view div 
+function listView(){
+  $(".grid-list-col").removeClass("col-md-6");
+  $(".grid-list-col").addClass("col-md-12");
+  $(".list-btn").addClass("active");
+  $(".grid-btn").removeClass("active");
+}
